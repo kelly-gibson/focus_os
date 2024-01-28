7fc516a5f875fd013ca74e0fa295b388edcbd853
carbon-wavelight <kjgibson@proton.me>
2024-01-27 17:18:22 -0800

    untracked files in gitignore

--- target/debug/deps/focus_os-ecb2a2f1a40669da.d
+++
@@ -1,5 +0,0 @@
-/home/axion/dev/rust/focus_os/target/debug/deps/libfocus_os-ecb2a2f1a40669da.rmeta: src/main.rs
-
-/home/axion/dev/rust/focus_os/target/debug/deps/focus_os-ecb2a2f1a40669da.d: src/main.rs
-
-src/main.rs:
