84cab5378616cb4574fc07434ae41b6cef3db39c
carbon-wavelight <kjgibson@proton.me>
2024-02-03 14:51:53 -0800

    Add config.toml: target runner for cargo build to boot into QEMU when using cargo run

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-// disabling the standard library
+// disables the standard library
 #![no_std]
-// telling the compiler to not use the normal entry point chain
+// tells the compiler to not use the normal entry point chain
 #![no_main]
 
 use core::panic::PanicInfo;
@@ -11,8 +11,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Welcome to focus OS";
+
 // Creating an entry point. Also tells the compiler to use the C calling convention, rather than the rust convention.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+    loop{}
 }
