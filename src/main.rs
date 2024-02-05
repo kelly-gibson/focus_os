0aefbf9cbdecb616fff6745d286422095d3c2efd
carbon-wavelight <kjgibson@proton.me>
2024-02-04 22:18:58 -0800

    Add: basic writer for VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 // tells the compiler to not use the normal entry point chain
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 // This function is called on panic
@@ -25,4 +27,17 @@ pub extern "C" fn _start() -> ! {
         }
     }
     loop{}
+}
+
+impl Writer {
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
 }
